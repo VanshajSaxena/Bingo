e1c4d36a1afa27d8c6d21219e872b374894524b5
Vanshaj Saxena <vs110405@outlook.com>
2024-09-20 23:57:57 +0530

    implemented hasWonGame and sub-methods

--- NewGame.java
+++ NewGame.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -18,29 +19,78 @@ public class NewGame {
   }
 
   private boolean hasWonGame() {
-    boolean numberOfBingoEqualsFive = false;
+    boolean scoredRequiredNoOfBingos = false;
     int numberOfBingos = 0;
-    int numberOfDiagonalBingos;
-    int numberOfVerticalBingos;
     int numberOfHorizontalBingos;
+    int numberOfVerticalBingos;
+    int numberOfDiagonalBingos;
 
-    checkDiagonalBingos();
-    checkVerticalBingos();
-    checkHorizontalBingos();
+    numberOfHorizontalBingos = checkHorizontalBingos();
+    numberOfVerticalBingos = checkVerticalBingos();
+    numberOfDiagonalBingos = checkDiagonalBingos();
 
-    return numberOfBingoEqualsFive;
+    numberOfBingos = numberOfHorizontalBingos + numberOfVerticalBingos + numberOfDiagonalBingos;
+
+    if (numberOfBingos == currentMatrixSize) {
+      scoredRequiredNoOfBingos = true;
+    }
+
+    return scoredRequiredNoOfBingos;
   }
 
-  private void checkVerticalBingos() {
-
+  private int checkHorizontalBingos() {
+    int horizontalBingos = 0;
+    for (int i = 0; i < currentMatrix.length; i++) {
+      if (Arrays.stream(currentMatrix[i]).allMatch(element -> element == -1)) {
+        horizontalBingos++;
+      }
+    }
+    return horizontalBingos;
   }
 
-  private static void checkDiagonalBingos() {
-
+  private int checkVerticalBingos() {
+    int verticalBingos = 0;
+    // currentMatrix[0].length gives the number of columns
+    for (int j = 0; j < currentMatrix[0].length; j++) {
+      boolean allMinusOne = true;
+      // this loops through number of rows, which currentMatrix.length returns
+      for (int i = 0; i < currentMatrix.length; i++) {
+        if (currentMatrix[i][j] != -1) {
+          allMinusOne = false;
+          break;
+        }
+      }
+      if (allMinusOne) {
+        verticalBingos++;
+      }
+    }
+    return verticalBingos;
   }
 
-  private static void checkHorizontalBingos() {
+  private int checkDiagonalBingos() {
+    int diagonalBingos = 0;
+    boolean mainDiagonalBingo = true;
+    boolean antiDiagonalBingo = true;
+    int length = currentMatrix.length;
 
+    for (int i = 0; i < length; i++) {
+
+      if (currentMatrix[i][i] != -1) {
+        mainDiagonalBingo = false;
+      }
+
+      if (currentMatrix[i][length - i - 1] != -1) {
+        antiDiagonalBingo = false;
+      }
+    }
+    if (mainDiagonalBingo) {
+      diagonalBingos++;
+    }
+
+    if (antiDiagonalBingo) {
+      diagonalBingos++;
+    }
+    return diagonalBingos;
   }
 
   private Matrix matrix;
